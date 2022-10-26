a16e061d36eb54ceb2c44eeeae0198aef628c8cb
Federico Volponi <federicovolponi99@gmail.com>
2022-10-26 16:25:39 +0200

    calculator class

--- LAB01/calculator.py
+++ LAB01/calculator.py
@@ -1,4 +1,6 @@
-
+from decimal import DivisionByZero
+import json
+import numpy as np
 
 class calculator():
 
@@ -6,31 +8,98 @@ class calculator():
         self.input = userInput
         self.command = ""
         self.values = []
+        self.subTrue = False
+        self.mulTrue = False
+        self.sumTrue = False
+        self.divTrue = False
+        self.exitTrue = False
         return
 
-    def add(self, to_addVect):
-        pass
+    def add(self):
+        if self.sumTrue:
+            res = np.sum(self.values)
+            addDictionary = {"input": self.values,
+                             "Operator": "sum",
+                             "output": res}
+            print(np.sum(self.values))
 
+    def sub(self):
+        if self.subTrue:
+            print(self.values[0] - sum(self.values[1:]))
     
+    def mul(self):
+        if self.mulTrue:
+            res = 1
+            for x in self.values:
+                res = res * x
+            print(res)
+    
+    def div(self):
+        if self.divTrue:
+            res = self.values[0]
+            for x in self.values[1: len(self.values)]:
+                try:
+                    res = res/x
+                except ZeroDivisionError:
+                    print("Cannot divide!")
+                    res = "Error"
+                    break
+            print(res)
+
     def iscommand(self, input):
-        if input == "sum":
-            return True
+        if input == "add":
+            self.subTrue = False
+            self.mulTrue = False
+            self.sumTrue = True
+            self.divTrue = False
+            self.exitTrue = False
         elif input == "sub":
-            return True
+            self.subTrue = True
+            self.mulTrue = False
+            self.sumTrue = False
+            self.divTrue = False
+            self.exitTrue = False
         elif input == "mul":
-            return True
+            self.subTrue = False
+            self.mulTrue = True
+            self.sumTrue = False
+            self.divTrue = False
+            self.exitTrue = False
         elif input == "div":
-            return True
+            self.subTrue = False
+            self.mulTrue = False
+            self.sumTrue = False
+            self.divTrue = True
+            self.exitTrue = False
         elif input == "exit":
-            return True 
-        else:
-            return False    
+            self.subTrue = False
+            self.mulTrue = False
+            self.sumTrue = False
+            self.divTrue = False
+            self.exitTrue = True
+        return    
 
 
 if __name__ == "__main__":
-    userInput = input()
-    c = calculator(userInput)
-    takevect = False
-    for input in c.input:
-        if input.isnumeric:
-            vect= vect.append(float(input))
+    flag = True
+    while flag:
+        user_input = input('Enter a operation: ')
+
+        c = calculator(user_input)
+        vect = []
+        for word in (c.input.split()+(["EOF"])):
+            isdigit = word.isdigit()
+            if isdigit:
+                vect.append(float(word))
+            else:
+                if len(vect) != 0:
+                    c.values = vect
+                    c.add()
+                    c.sub()
+                    c.div()
+                    c.mul()
+                    vect = []
+                c.iscommand(word)
+                flag = not c.exitTrue
+
+    
