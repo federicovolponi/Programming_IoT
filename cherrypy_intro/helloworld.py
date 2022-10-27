706d5c581ee82a65b5459fb65b9521a8605b88bc
Federico <federicovolponi99@gmail.com>
2022-10-27 17:24:04 +0200

    cherrypy

--- cherrypy_intro/helloworld.py
+++ cherrypy_intro/helloworld.py
@@ -1,30 +1,37 @@
 import cherrypy
 class HelloWorld(object):
-    exposed=True
-    def GET(self,*uri,**params):
-        #Standard output
-        output="Hello World"
-        #Check the uri in the requests
-        #<br> is just used to append the content in a new line
-        #<br> is the \n for HTML)
-        if len(uri)!=0:
-            output+='<br>uri: '+','.join(uri)
-        #Check the parameters in the request
-        #<br> is just used to append the content in a new line
-        #<br> is the \n for HTML)
-        if params!={}:
-            output+='<br>params: '+str(params)
-        return output
+	exposed=True
+	def GET(self,*uri,**params):
+		#Standard output
+		output="Hello World"
+		#Check the uri in the requests
+		#<br> is just used to append the content in a new line
+		#<br> is the \n for HTML)
+		if len(uri)!=0:
+			str_uri = uri[0]
+			output += '<br>' + f"{str_uri[::-1]}" 
+			
+		#Check the parameters in the request
+		#<br> is just used to append the content in a new line
+		#<br> is the \n for HTML)
+		if params!={}:
+			output+='<br>params: '+str(params)
+
+		
+		return output
+
+	def POST(self, *uri, **params):
+		
 
 if __name__ == "__main__": #Standard configuration to serve the url "localhost:8080"
-    
-    conf={
-        '/':{
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'tool.session.on': True
-        }
-    }
-    webService=HelloWorld()
-    cherrypy.tree.mount(webService,'/',conf)
-    cherrypy.engine.start()
-    cherrypy.engine.block()
+	
+	conf={
+		'/':{
+			'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+			'tool.session.on': True
+		}
+	}
+	webService=HelloWorld()
+	cherrypy.tree.mount(webService,'/',conf)
+	cherrypy.engine.start()
+	cherrypy.engine.block()
